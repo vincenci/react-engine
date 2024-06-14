05907ed559c39310d06309127d0a0776ea51b482
vincenci <vincyvine558@gmail.com>
2024-06-14 07:09:54 +0300

    created and added react engine html and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Weather from './weather';
 
 function App() {
   return (
+  <div className='weatherProject'>
     <div className="App">
-      return(weather)
+      <Weather defaultCity="Nairobi" />
+     
+      <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/vincenci"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vincencia
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/vincenci/weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://lovely-pika-675f60.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
     </div>
+    </div>
+    
   );
 }
 
---
+++ src/WeatherInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FormattedDate from "./formattedDate";
+import WeatherIcon from "./WeatherIcons";
+import WeatherTemperature from "./WeatherTemperature";
+export default function WeatherInfo(props){
+    return(
+       <div className="WeatherInfo">
+      
+      
+      <div className="row mt-3">
+        <div className="col-6">
+          
+           <h1>{props.data.city}</h1>
+      
+        <span>
+          <FormattedDate date={props.data.date}/>
+        </span>
+        <span className="text-capitalize">{props.data.description}</span>
+        <br/>
+          
+            <span>Humidity: <strong>{props.data.humidity}%</strong></span>
+            <span>Wind: <strong>{props.data.wind} km/h</strong> </span> 
+          
+        </div>
+        <div className="col-6">
+          <div className="d-flex">
+          <div>
+             <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+            </div>
+        </div>
+      </div>
+    </div> 
+    )
+}
